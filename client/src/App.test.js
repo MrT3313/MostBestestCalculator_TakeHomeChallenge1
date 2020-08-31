1bd1b9c9f5a15119675628b92fdd99546cc865db
MrT3313 <turgeon.dev@gmail.com>
2020-08-31 16:44:36 -0400

    Initial <App /> tests
    1. <App /> renders without crashing
    2. <App /> queryByText => Hello World => toBeTruthy

---
+++ client/src/App.test.js
@@ -0,0 +1,37 @@
+// // IMPORTS
+// import React from 'react'
+// import { render } from '@testing-library/react'
+
+// // COMPONENTS
+// import { App } from './app.js'
+
+// if('Renders without crashing', () => {
+
+// })
+
+// -- -- -- -- -- -- -- -- //
+// -- -- -- -- -- -- -- -- //
+// -- -- -- -- -- -- -- -- //
+
+// IMPORTS
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+
+// COMPONENTS
+import App from './App'
+
+// __MAIN__
+// - 1 - // Cleanup
+afterEach(cleanup)
+
+// - 2 - // Tests
+it('renders without crashing', () => {
+  render(<App />)
+})
+test('check for Hello World text', () => {
+    const wrapper = render(<App />)
+    // console.log(wrapper.debug())
+
+    const element = wrapper.queryByText(/hello world/i)
+    expect(element).toBeTruthy()
+})
